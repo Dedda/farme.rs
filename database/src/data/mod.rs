aa264088749865b908425c5cc996bdcb3a2d8778
Stephan Göppentin <dedda102@gmail.com>
2025-11-04 21:18:43 +0100

    FRM-220: Removed dangling empty mod

--- database/src/data/mod.rs
+++
@@ -1,9 +0,0 @@
-use rocket::fairing::AdHoc;
-use rocket::{Build, Rocket};
-use rocket_sync_db_pools::database;
-use rocket_sync_db_pools::diesel;
-
-pub mod farm;
-pub mod user;
-pub mod location;
-
